9903da5ad7796a0af81419b922fc287eda2130e5
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-06 15:23:09 +0200

    navbar

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,14 +6,21 @@ export const NewsBoard = () => {
 
    useEffect(()=>{
    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
-   fetch(url).then(response => response.json()).then(data=>setArticles(data.articles))
+   fetch(url)
+   .then(response => response.json())
+   .then(data=>setArticles(data.articles))
   },[])
 
   return (
     <div>
       <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>
       {articles.map((news,index)=>{
-        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} author={news.author}/>
+        return <NewsItem key={index} 
+        title={news.title} 
+        description={news.description} 
+        src={news.urlToImage} 
+        url={news.url} 
+        author={news.author}/>
       })
       }
     </div>

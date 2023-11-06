82eab686445b0fb7c7ff0b0604a872c51b354393
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-06 14:36:37 +0200

    gitignore

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react"
+
 export const NewsBoard = () => {
+  const [articles,setArticles] = useState([]);
+
+   useEffect(()=>{
+   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
+   })
+
   return (
     <div>
       <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>

8f36647ed477d289405bd5d777697b219ba92efe
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-06 14:53:13 +0200

    newsBoard

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react"
+import { NewsItem } from "./NewsItem";
 
 export const NewsBoard = () => {
   const [articles,setArticles] = useState([]);
 
    useEffect(()=>{
-   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
-   })
+   let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
+   fetch(url).then(response => response.json()).then(data=>setArticles(data.articles))
+  },[])
 
   return (
     <div>
       <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>
+      {articles.map((news,index)=>{
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} author={news.author}/>
+      })
+      }
     </div>
   )
 }
--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
-export const NewsItem = () => {
+export const NewsItem = ({title,description,src,url}) => {
   return (
-    <div>NewsItem</div>
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+    <img src={src} className="card-img-top" alt="..." style={{height:"200px"}}/>
+    <div className="card-body">
+      <h5 className="card-title">{title.slice(0,50)}</h5>
+      <p className="card-text">{description?description.slice(0,90):"No Descript Provided"}</p>
+      <a href={url} className="btn btn-primary">Read More</a>
+    </div>
+  </div>
   )
 }
